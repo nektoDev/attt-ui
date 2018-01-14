6381cce8282732e9f666943766d7304cb1fc4634
nektodev <ts.slawa@gmail.com>
2018-01-14 20:51:53 +0300

    Move service URL to env config

--- src/app/service/torrent.service.ts
+++ src/app/service/torrent.service.ts
@@ -2,20 +2,23 @@ import { Injectable } from '@angular/core';
 import {Torrent} from '../model/torrent';
 import {Observable} from 'rxjs/Observable';
 import {HttpClient} from '@angular/common/http';
-import {TorrentAddRequest} from "../model/torrent-add-request";
+import {TorrentAddRequest} from '../model/torrent-add-request';
+import {environment} from '../../environments/environment';
 
 @Injectable()
 export class TorrentService {
+
+  torrentURL = environment.attt_host + '/torrent';
 
   constructor(private http: HttpClient) {
 
   }
 
   getTorrents(): Observable<Torrent[]> {
-    return this.http.get<Torrent[]>('http://192.168.1.11:8081/attt/torrent');
+    return this.http.get<Torrent[]>(this.torrentURL);
   }
 
   addTorrent(torrent: TorrentAddRequest): Observable<any> {
-    return this.http.post('http://192.168.1.11:8081/attt/torrent', torrent);
+    return this.http.post(this.torrentURL, torrent);
   }
 }
